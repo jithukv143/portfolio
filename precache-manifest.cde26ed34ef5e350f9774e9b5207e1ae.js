181682b4dbad64d5dc529b8feb0c03c9aaa00f4c
jithukv143 <jithukrishnanv@gmail.com>
2020-08-30 15:09:42 +0530

    Updates

---
+++ precache-manifest.cde26ed34ef5e350f9774e9b5207e1ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c8bb9ec839f2979ce0d",
+    "url": "/portfolio/static/css/main.a69a1dde.chunk.css"
+  },
+  {
+    "revision": "3c8bb9ec839f2979ce0d",
+    "url": "/portfolio/static/js/main.3c8bb9ec.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "a805318dc361e7efdf4d91f4f848bbad",
+    "url": "/portfolio/index.html"
+  }
+];
